8c869a38fa2ee412ba0f7f49c6c989592aa52e99
Kris Logemann <krisrlogemann@gmail.com>
2019-06-21 11:02:35 -0700

    adds initial project files

---
+++ hackathon/src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './App.css';
+const axios = require('axios');
+
+export default class App extends Component {
+  constructor(props) {
+  super(props);
+    this.state = {
+      randomKanye: '',
+      randomDog: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.getKanye = this.getKanye.bind(this);
+    this.getDog = this.getDog.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  submit() {
+    this.getKanye();
+    this.getDog();
+  }
+
+  getKanye() {
+    const reqURL = 'https://api.kanye.rest/';
+    axios.get(reqURL).then(res => {
+      this.setState({
+        randomKanye: res.data.quote
+      });
+    });
+  }
+
+  getDog() {
+    const reqURL2 = 'https://dog.ceo/api/breeds/image/random';
+    axios.get(reqURL2).then(res => {
+      this.setState({
+        randomDog: res.data.message
+      });
+    });
+  }
+
+render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='App-title'>Hackathon Random App</h1>
+        <p className='App-intro'>Get a Random Kanye West Quote and Dog Picture at the Click of a Button!</p>
+        <button
+        id='getInfo'
+        className='btn btn primary'
+        onClick={this.submit}
+        >
+          Click Here
+        </button>
+      </header>
+      <div className='container'>
+        <div className='body'>
+          <div
+            name='quote'
+            id='quote'
+            className='container-fluid'
+            onChange={this.handleChange}
+            >
+              Kanye Says:
+              {this.state.randomKanye}
+            </div>
+            <div
+            message='success'
+            className='container-fluid'
+            onChange={this.handleChange}
+            >
+              Dog Pic:
+              <img src={this.state.randomDog}/>
+            </div>
+        </div>
+      </div>
+      </div>
+    );
+  }
+}
